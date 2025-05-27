f09528521a63ed0f6cad4ea3e503c165659595d6
Andrea Cappelli <kattasc@gmail.com>
2025-05-27 23:22:06 +0200

    add test command

---
+++ try_vad.py
@@ -0,0 +1,16 @@
+import soundfile as sf
+
+from vad import SileroVADOnnx, VADIterator
+
+
+if __name__ == "__main__":
+    vad_model = SileroVADOnnx()
+
+    audio, sr = sf.read('recorded.wav', dtype='float32')
+    vad_iterator = VADIterator(vad_model)
+
+    for i in range(0, len(audio), 512):
+        chunk = audio[i:i + 512]
+        result = vad_iterator(chunk, return_seconds=True)
+        if result:
+            print(result)
