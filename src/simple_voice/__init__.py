df1a99b7174f4dae3ef66f5702b6749c17b26d0c
Andrea Cappelli <kattasc@gmail.com>
2025-08-02 19:37:40 +0200

    adapt init

--- src/simple_voice/__init__.py
+++ src/simple_voice/__init__.py
@@ -1,2 +1,7 @@
+from pathlib import Path
+
+ASSETS_DIR = Path(__file__).parents[0] / "assets"
+
 from .vad import SileroVADOnnx, VADIterator
+from .stt import Moonshine
 from .simple_voice import Listener
